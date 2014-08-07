87a90cebd6ce3f9f377ef9b62a77909d8197686a
ronniebermejo <ronnie.bermejo.mx@gmail.com>
2014-08-06 23:31:28 -0500

    Application helper for loading Angular templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 # # Render a partial into a script tag so Angular
 # # sticks it into $templateCache
   def load_ng_template(partial)
-    content_tag :script, type: 'text/ng-template', id: "#{partial}.html" do
+    content_tag :script, type: 'text/ng-template', id: "#{partial}.html", controller: "findMyDogApp" do
       render partial
     end
   end
